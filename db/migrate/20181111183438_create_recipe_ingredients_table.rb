5bbff634e24574ca84b7e642ce2c60de2b1607ba
Philip Larie <philiprlarie@gmail.com>
2018-11-11 10:54:37 -0800

    Add an ingredients table. Create a many to many relationship between ingredients and recipes.
    https://guides.rubyonrails.org/association_basics.html#the-has-many-through-association
    
    https://www.youtube.com/watch?v=Cj1FhPy9sz4

---
+++ db/migrate/20181111183438_create_recipe_ingredients_table.rb
@@ -0,0 +1,11 @@
+class CreateRecipeIngredientsTable < ActiveRecord::Migration[5.2]
+  def change
+    create_table :recipe_ingredients do |t|
+      t.belongs_to :recipe, index: true
+      t.belongs_to :ingredient, index: true
+      t.integer :units
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_11_031359) do
+ActiveRecord::Schema.define(version: 2018_11_11_183438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "recipe_ingredients", force: :cascade do |t|
+    t.bigint "recipe_id"
+    t.bigint "ingredient_id"
+    t.integer "units"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["ingredient_id"], name: "index_recipe_ingredients_on_ingredient_id"
+    t.index ["recipe_id"], name: "index_recipe_ingredients_on_recipe_id"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name", null: false
