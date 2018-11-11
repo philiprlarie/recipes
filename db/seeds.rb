d0df7aa66bfda0aae19021fd8248c474827a237e
Philip Larie <philiprlarie@gmail.com>
2018-11-11 09:47:51 -0800

    Add seed dump to fill seed file from dev database
    https://github.com/rroblak/seed_dump

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Recipe.create!([
+  {name: "Smores", steps: "put marsh over the fire. put that on grahm, and add the chocolate", source: "My head"}
+])
