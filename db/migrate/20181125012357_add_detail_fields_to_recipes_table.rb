69c4b6327ceb95cf29c8dd59dc58c56208c33317
Philip Larie <philiprlarie@gmail.com>
2018-11-24 17:30:52 -0800

    Add detail columns to recipes table

---
+++ db/migrate/20181125012357_add_detail_fields_to_recipes_table.rb
@@ -0,0 +1,8 @@
+class AddDetailFieldsToRecipesTable < ActiveRecord::Migration[5.2]
+  def change
+    add_column :recipes, :prep_time,      :integer, null: false, default: 0
+    add_column :recipes, :cook_time,      :integer, null: false, default: 0
+    add_column :recipes, :inactive_time,  :integer, null: false, default: 0
+    add_column :recipes, :servings,       :float,   null: false, default: 0
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_25_001922) do
+ActiveRecord::Schema.define(version: 2018_11_25_012357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,10 @@ ActiveRecord::Schema.define(version: 2018_11_25_001922) do
     t.string "source", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "prep_time", default: 0, null: false
+    t.integer "cook_time", default: 0, null: false
+    t.integer "inactive_time", default: 0, null: false
+    t.float "servings", default: 0.0, null: false
   end
 
 end
