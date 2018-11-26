797caeefccb8943a7008fb125b28e652b7a4bb5d
Philip Larie <philiprlarie@gmail.com>
2018-11-25 18:19:25 -0800

    Add a namespace for api urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  jsonapi_resources :recipes
+  # https://guides.rubyonrails.org/routing.html#controller-namespaces-and-routing
+  namespace :api, module: nil do
+    jsonapi_resources :recipes
+  end
 end
